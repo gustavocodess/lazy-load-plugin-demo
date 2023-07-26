c3df02c510096f579560a4518ed2209382d3c336
gustavocodess <gustavoh.g.s@hotmail.com>
2023-07-26 01:36:10 -0300

    added logic for custom header

---
+++ src/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Header: React.FC = () => {
+  return (
+    <div style={{ background: 'red', width: '100%' }}>
+      This is a test header
+    </div>
+  );
+}
+
+export default Header;
--- src/plugin.tsx
+++ src/plugin.tsx
@@ -1,9 +1,11 @@
 /** @jsx jsx */
 import React from 'react';
+import { reaction } from 'mobx'
 import { jsx } from '@emotion/core';
 import { Builder } from '@builder.io/sdk';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import Header from './Header'
 
 
 
@@ -57,10 +59,47 @@ Builder.registerEditor({
 });
 
 
-Builder.registerEditor({
-  /**
-   * Here we override the built-in richtext editor.
-   */
-  name: 'other html',
-  component: React.lazy(() => System.import('./RegisterDefault')),
-});
+// Builder.registerEditor({
+//   /**
+//    * Here we override the built-in richtext editor.
+//    */
+//   name: 'other html',
+//   component: React.lazy(() => System.import('./RegisterDefault')),
+// });
+const pluginId = 'my-custom-plugin'
+Builder.register('plugin', {
+  // should match npm id if loading from npm
+  id: pluginId,
+})
+
+function CustomButton() {
+  return (
+    <div>
+      <button name='btn' onClick={() => alert('clicked')}>
+        Click me
+      </button>
+    </div>
+  )
+}
+
+reaction(
+  () => true,
+  (advancedMode) => {
+    Builder.register('editor.header', { component: Header });
+    Builder.register('editor.settings', {
+      // hideHeatMap: true,
+      hideCommentsTab: true,
+      // hideGetFeedbackLink: true,
+      hideHelpWidget: true,
+      // hideMainTabs: false,
+      // hideOptionsTab: true,
+    })
+    // Builder.register('editor.header', {
+    //   component: CustomButton,
+    // })
+    
+  },
+  {
+    fireImmediately: true,
+  }
+)
